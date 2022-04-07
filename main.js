f7255c8e82be9cf98f2c9b32a34b7d46824eb951
Diogo <diogocalmons@gmail.com>
2022-04-07 09:38:50 -0300

    Adicionando responsividade ao menu

---
+++ main.js
@@ -0,0 +1,11 @@
+let menuList = document.getElementById("menulist")
+menuList.style.maxHeight = "0px";
+function togglemenu(){
+
+    if(menuList.style.maxHeight == "0px"){
+        menuList.style.maxHeight = "130px";
+    }   
+    else{
+        menuList.style.maxHeight = "0px";
+    }
+}
